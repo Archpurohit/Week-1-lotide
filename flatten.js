1a1b528c1ab96b763fbb63afa19f835ecba54a96
Archit Purohit <111916382+Archpurohit@users.noreply.github.com>
2022-10-21 10:51:01 -0700

    Add files via upload

---
+++ flatten.js
@@ -0,0 +1,13 @@
+const flatten = function(inputArray) {
+  let answerArray = [];
+  for (let element of inputArray) {
+    if (Array.isArray(element)) {
+      answerArray.push(...element);
+    } else {
+      answerArray.push(element);
+    }
+  }
+  return answerArray;
+};
+
+module.exports = flatten;
