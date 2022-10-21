4e6ddd63f2db4aa9231bcd07459a8eae3f1d8c70
Archit Purohit <111916382+Archpurohit@users.noreply.github.com>
2022-10-21 13:41:59 -0700

    Add files via upload

---
+++ middle.js
@@ -0,0 +1,12 @@
+const middle = function(array) {
+  let array_result = []; //Initialize it to empty array
+  if (array.length <= 2 && array.length > 0) {
+    return array_result;
+  } else if (array.length % 2 === 0) {
+    return array_result = [array[(array.length / 2) - 1], array[array.length / 2]];
+  } else {
+    return array_result = array[(array.length - 1) / 2];
+  }
+};
+
+console.log(middle)
