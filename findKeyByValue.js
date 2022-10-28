481bd16281c7a6e1a5f11e54485e5118b5025c1f
Archit Purohit <111916382+Archpurohit@users.noreply.github.com>
2022-10-27 17:17:19 -0700

    Add files via upload

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed:  ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+}; 
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  action: "Bourne identity",
+  Romance: "The Notebook"
+};
+
+const findKeyByValue = function(genre,genrevalue){
+for(let key in genre){
+if (genrevalue === genre[key]){
+  console.log(key)
+  return true;
+}
+
+}
+return undefined; 
+
+};
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
