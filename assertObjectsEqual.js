c97cb5cf4be68dec7071fa826fa03536baedbb2d
Archit Purohit <111916382+Archpurohit@users.noreply.github.com>
2022-10-31 11:18:01 -0700

    Create assertObjectsEqual.js

---
+++ assertObjectsEqual.js
@@ -0,0 +1,9 @@
+const eqObjects = require('./eqObjects');
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; // <= add this line
+  if (eqObjects(inputtedObject, expectedObject)) {
+    console.log(`✅ Assertation Passed: ${inspect(inputtedObject)} === ${inspect(expectedObject)}`);
+  } else {
+    console.log(`🔴 Assertation Failed: ${inspect(inputtedObject)} !== ${inspect(expectedObject)}`);
+  };
