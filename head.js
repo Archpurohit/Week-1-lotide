8d3096e8c71efab2cab8904213b1a270bf093e77
Archit Purohit <111916382+Archpurohit@users.noreply.github.com>
2022-11-07 11:50:26 -0800

    Create head.js

---
+++ head.js
@@ -0,0 +1,8 @@
+assertEqual(head([45,43,73,21]), 45);
+assertEqual(head(["Goodmorning", "Lighthouse", "Labs"]), "goodmorning");
+
+const assertEqual = require('./assertEqual');
+
+} 
+
+module.exports = head;
