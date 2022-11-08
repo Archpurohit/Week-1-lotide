5c3b6583b375f3b9479d38976975f2b787dddb7d
Archit Purohit <111916382+Archpurohit@users.noreply.github.com>
2022-11-08 10:44:59 -0800

    Create tailTest.js

---
+++ tailTest.js
@@ -0,0 +1,17 @@
+const assertEqual = require('./assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
+const tail = function(arr){
+    if (arr.length < 2) {
+        return arr;
+    } else {
+    return arr.slice(4);
+    }   
+}
+console.log(tail([1,2,3,4,5]))
+module.exports = tail;
+
+it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
