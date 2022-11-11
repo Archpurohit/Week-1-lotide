2a32f8244c1bde46ecd1098d6e97f4e15828175c
Archit Purohit <111916382+Archpurohit@users.noreply.github.com>
2022-11-10 16:16:11 -0800

    Update countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion Passed:  ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
@@ -25,23 +19,16 @@ const countOnly = function(allItems, itemsToCount) {
 return results;
 }  
 
-module.exports = countOnly;
 
 const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
+  "Archit",
+  "Purohit",
+  "Santi",
 ];
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+const result1 = countOnly(firstNames, { "Archit": true, "Purohit": true, "Santi": true,});
 
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+assertEqual(result1["Archit"], 1);
+assertEqual(result1["Purohit"], undefined);
+assertEqual(result1["Santi"], 2);
+
