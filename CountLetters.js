daa3101b16f900aca139a3a9e303c352abcbe0eb
Archit Purohit <111916382+Archpurohit@users.noreply.github.com>
2022-11-10 16:13:36 -0800

    Update CountLetters.js

--- CountLetters.js
+++ CountLetters.js
@@ -1,4 +1,2 @@
 var s = "I am the best footballer in the world, Messi and Ronaldo can't compete with me";
-var result = [...s].reduce((a, e) => { a[e] = a[e] ? a[e] + 1 : 1; return a }, {}); 
-console.log(result);
-
+console.log(s.length);
