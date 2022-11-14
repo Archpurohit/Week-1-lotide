93b23b3579cfdcbc53c6282976bea33e78138ebf
Archit Purohit <111916382+Archpurohit@users.noreply.github.com>
2022-11-14 09:57:02 -0800

    Update CountLetters.js

--- CountLetters.js
+++ CountLetters.js
@@ -1,2 +1,15 @@
-var s = "I am the best footballer in the world, Messi and Ronaldo can't compete with me";
-console.log(s.length);
+const countLetters = function(sentence) {
+let numberedLetters = [];
+let string = sentence.split(" ").join("");
+
+for (const characters of string) {
+if (numberedLetters[characters]){
+  numberedLetters[characters] += 1;
+} else{
+  numberedLetters = 1;
+ }
+}
+  return numeredLetters;
+};
+
+module.exports = countLetters
